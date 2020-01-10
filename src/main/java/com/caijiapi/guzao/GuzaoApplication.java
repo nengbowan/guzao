630801b4808c74143dd785fb644fabfd3abd2b4f
傅士勇 <nengbowan@163.com>
2020-01-10 12:52:48 +0800

    Initial commit

---
+++ src/main/java/com/caijiapi/guzao/GuzaoApplication.java
@@ -0,0 +1,13 @@
+package com.caijiapi.guzao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuzaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuzaoApplication.class, args);
+    }
+
+}
